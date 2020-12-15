c7f209788b4686e0953db1335f7d1e86bce35f6a
rolanduwxcc <rolanduwxcc@gmail.com>
2020-12-15 00:05:23 -0600

    can now attack multiple robots

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,23 +1,9 @@
-// // window refers to the browser, it is always present when run in browser
-// //window.alert("This is an alert! javaScript is running!");
+//Game States
+// "WIN" - Player robot has defeated all enemy robots
+//  * Fight all enemy-robots
+//  * Defeat each enemy-robot
+// "LOSE" - Player robot's health is zero or less
 
-
-// //this creates a fight function
-// function fight() {
-//     window.alert("The fight has begun!");
-// }
-
-// var playerName = window.prompt("What is your robot's name?");
-// //window.alert(playerName);
-// //what is this?
-// console.log(playerName);
-// console.log("this logs a string, good for leaving yourself a message");
-// //this will do math and log 17
-// console.log(10+7);
-// // what is this?
-// console.log("Our robot's name is " + playerName)
-
-// //fight();
 
 var playerName = window.prompt("What is your robot's name?");
 var playerHealth = 100;
@@ -27,11 +13,11 @@ var playerMoney = 10;
 //You can also log multiple values at once like this
 console.log(playerName, playerName, playerHealth);
 
-var enemyName = "Roborto";
+var enemyNames = ["Roborto", "Amy Android", "Robo Fet"];
 var enemyHealth = 50;
 var enemyAttack = 12;
 
-var fight = function() {
+var fight = function(enemyName) {
     // Alert players that they are starting the round
     window.alert("Welcome to Robot Gladiators!");
 
@@ -88,10 +74,10 @@ var fight = function() {
     }
     else {
         window.alert("You need to choose a valid option. Try again!");
-    }
-    
-
-
+    }  
 };
 
-fight();
+for (var i=0; i < enemyNames.length; i++) {
+    fight(enemyNames[i]);
+}
+//fight();
