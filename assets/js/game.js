d92881249cd15c73059ee5389b48fe98ad36cf23
rolanduwxcc <rolanduwxcc@gmail.com>
2020-12-19 01:01:24 -0600

    update the menu picker for store)
    q
    exit

--- assets/js/game.js
+++ assets/js/game.js
@@ -114,22 +114,20 @@ var endGame = function() {
 };
 
 var shop = function() {
-    var shopOptionPrompt = window.prompt("Would you like to REFILL your health, UPGRADE your attack, or LEAVE the store? Please enter one: 'REFILL', 'UPGRADE', or 'LEAVE' to make a choice.");
+    var shopOptionPrompt = window.prompt("Would you like to (1) REFILL your health, (2) UPGRADE your attack, or (3) LEAVE the store? Please enter a number : 1, 2 or 3 to make a choice.");
     if (shopOptionPrompt === null) {
         shopOptionPrompt = "x";
     }
     shopOptionPrompt = shopOptionPrompt.toLocaleLowerCase();
     //switcharoo
     switch(shopOptionPrompt) {
-        case "REFILL":
-        case "refill":
+        case "1":
             playerInfo.refillHealth();
             break;
-        case "UPGRADE":
-        case "upgrade":
+        case "2":
             playerInfo.upgradeAttack();
             break;
-        case "leave":
+        case "3":
             window.alert("Leaving the store.");
             //do nothing, function ends
             break;
